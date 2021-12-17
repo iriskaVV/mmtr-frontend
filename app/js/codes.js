0fddac129ba41bedb41d2b14cff35292fd2a9d56
iriskaVV <vorotyntseva02iris@mail.ru>
2021-12-17 16:45:50 +0300

    ссылка на service блок (js)
    при нажатии на кнопку в меню пролистывается страница до нужного блока

--- app/js/codes.js
+++ app/js/codes.js
@@ -1,8 +1,9 @@
-
+debugger
 // переход на servicec-block
-$(document).ready(function () {
-    $("a").click(function () {
-        let elementClick = $(this).attr("href");
-        let destination = $(elementClick).offset().top; 
+let serviceFormLink = document.getElementById('serviceForm');
+
+serviceFormLink.onclick = function link() {
+    document.getElementById('servicesBlock').scrollIntoView({
+        behavior: 'smooth'
     });
-});
+  }
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,38 @@
 var gulp    = require('gulp'), // Подключаем Gulp
-sass        = require('gulp-sass')(require('sass')), //Подключаем Sass пакет,
+sass        = require('gulp-sass')(require('sass')),
 browserSync = require('browser-sync'); // Подключаем Browser Sync
 
 gulp.task('sass', function(){ // Создаем таск Sass
-return gulp.src('app/sass/*.sass', {allowEmpty: true}) // Берем источник
+return gulp.src('app/sass/*.scss', {allowEmpty: true}) // Берем источник
     .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-    .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+    .pipe(gulp.dest('app/css'))  // Выгружаем результата в папку app/css
     .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-browserSync({ // Выполняем browserSync
-    server: { // Определяем параметры сервера
-        baseDir: 'app' // Директория для сервера
-    },
-    notify: false // Отключаем уведомления
+gulp.task('scripts', function() {
+  return gulp.src('app/js/*.js', {allowEmpty: true})
+  .pipe(browserSync.reload({ stream: true }))
 });
+ 
+gulp.task('code', function() {
+  return gulp.src('app/*.html', {allowEmpty: true})
+  .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('watch', function() {
-gulp.watch('app/sass/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами
+    gulp.watch('app/sass/*.scss', gulp.parallel('sass')); // Наблюдение за sass файлами
+    gulp.watch('app/*.html', gulp.parallel('code')); // Наблюдение за HTML файлами
+    gulp.watch('app/js/*.js', gulp.parallel('scripts')); // Наблюдение за js файлами
+    });
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера
+        },
+        notify: false // Отключаем уведомления
+    });
 });
-gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+
+gulp.task('default', gulp.parallel('sass', 'scripts', 'code', 'browser-sync', 'watch'));
  
